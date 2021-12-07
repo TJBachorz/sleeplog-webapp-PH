bd4271313ab61b44edc29744b4dad9ab0a75faef
TJ <tjbachorz@gmail.com>
2021-12-07 12:32:05 -0600

    removes unnecessary Select.js fields

--- src/Reusables/Select.js
+++ src/Reusables/Select.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import { Field } from 'formik';
 
-const Select = ({
-    name,
-    options,
-    type,
-}) => {
+const Select = ({ name, options, type }) => {
+
     return (
-        <Field
-            as="select"
-            name={name}
-            type={type}
-        >
+        <Field as="select" name={name} type={type}>
             {options.map((option, index) => {
                 return (
                     <option key={index} value={option}>{option}</option>
