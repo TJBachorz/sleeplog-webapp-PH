c9df4b1a82e85a0cae0f345eec4a28a018074f42
TJ <tjbachorz@gmail.com>
2021-12-07 12:12:17 -0600

    fixes formik, selects now preload and respond to change

--- src/Reusables/Select.js
+++ src/Reusables/Select.js
@@ -3,18 +3,14 @@ import { Field } from 'formik';
 
 const Select = ({
     name,
-    onChange,
     options,
     type,
-    value
 }) => {
     return (
         <Field
             as="select"
             name={name}
-            onChange={onChange}
             type={type}
-            value={value}
         >
             {options.map((option, index) => {
                 return (
--- src/components/SleepForm.js
+++ src/components/SleepForm.js
@@ -7,44 +7,39 @@ import dayjs from 'dayjs';
 
 const SleepForm = () => {
 
-    console.log(yesterday.year())
-
-    const formik = useFormik({
-        initialValues: {
-            nightOfDateMonth: '',
-            nightOfDateDay: '',
-            nightOfDateYear: '',
-            bedtime: '',
-            sleepDate: '',
-            approximateSleepTime: '',
-            sleepInterrupted: 0,
-            sleepLostFromInterruptions: 0,
-            wakeUpDate: '',
-            wakeUpTime: '',
-            ateSpicy: false,
-            drankAlcohol: false,
-            useMarijuana: false,            
-            sleepMeds: false,
-            userId: '',
-            notes: '',
-        },
-    })
+    const initialValues = {
+        nightOfDateMonth: yesterday.month() + 1,
+        nightOfDateDay: '',
+        nightOfDateYear: '',
+        bedtime: '',
+        sleepDate: '',
+        approximateSleepTime: '',
+        sleepInterrupted: 0,
+        sleepLostFromInterruptions: 0,
+        wakeUpDate: '',
+        wakeUpTime: '',
+        ateSpicy: false,
+        drankAlcohol: false,
+        useMarijuana: false,            
+        sleepMeds: false,
+        userId: '',
+        notes: '',
+    }
 
     const submitSleeplog = values => {
-        console.log(values)
         console.log(values, "SUBMITTING SLEEPLOG")
     }
 
     return (
         <Formik
-            initialValues={formik.initialValues}
+            initialValues={initialValues}
             onSubmit={submitSleeplog}
         >
             {props => (<form onSubmit={props.handleSubmit}>
                 <div>
-                    <Select name="nightOfDateMonth" onChange={props.handleChange} type="text" value={12} options={[1,2,3,4,5,6,12]}/>
-                    <Select name="nightOfDateDay" type="text" value={20} options={[1,2,3,4,5,30]}/>
-                    <Field name="nightOfDateYear" type="text" value={yesterday.year()}/>
+                    <Select name="nightOfDateMonth" type="text" options={[1,2,3,4,5,6,7,8,9,10,11,12]}/>
+                    {/* <Field name="nightOfDateDay" type="text" value={20} options={[1,2,3,4,5,30]}/>
+                    <Field name="nightOfDateYear" type="text" value={yesterday.year()}/> */}
                 </div>
                 <button type="submit">Submit</button>
             </form>)}
